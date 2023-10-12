93951fb43555cab7e62d764fccd5610c4f3cb06b
hrfmmr <i@hrfmmr.com>
2023-10-12 15:35:13 +0900

    Add server

---
+++ 202310/cmd/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+
+	hellopb "mygrpc/pkg/grpc"
+)
+
+type myGreetingServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func (s *myGreetingServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
+
+func NewMyGreetingServer() *myGreetingServer {
+	return &myGreetingServer{}
+}
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	hellopb.RegisterGreetingServiceServer(s, NewMyGreetingServer())
+
+	go func() {
+		log.Printf("💨 Start gRPC server port:%+v\n", port)
+		s.Serve(listener)
+	}()
+
+	q := make(chan os.Signal, 1)
+	signal.Notify(q, os.Interrupt)
+	<-q
+	log.Println("👋 Stopping gRPC server")
+	s.GracefulStop()
+}
